3bfed958cb1775a15dec916e2aed445b74746361
Adrian Bercea <adrian.bercea@autouncle.com>
2025-02-02 16:24:35 +0200

    Add auth with Devise

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show ]
   before_action :set_task, only: %i[ show edit update destroy ]
 
   # GET /tasks or /tasks.json
