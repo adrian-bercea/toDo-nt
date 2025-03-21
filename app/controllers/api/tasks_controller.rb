5d37f96fe90857eda24495a91800530e324140fb
Adrian Bercea <adrian.bercea@autouncle.com>
2025-03-21 19:01:22 +0200

    API task controller add offset and limit support

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,9 +1,16 @@
 module Api
   class TasksController < ApplicationController
+    MAX_LIMIT     = 50
+    DEFAULT_LIMIT = 24
+    DEFAULT_OFFSET = 0
+
     protect_from_forgery unless: -> { request.format.json? } # CSRF protection is disabled for JSON requests, no bueno but just for giggles
 
     def index
-      @tasks = Task.includes(:categories).all
+      @tasks = Task.includes(:categories)
+                 .offset(current_offset)
+                 .limit(current_limit)
+
       render json: tasks_collection_json
     end
 
@@ -56,6 +63,16 @@ module Api
 
     def task_params
       params.require(:task).permit(:title, :description, :completed, category_ids: [])
+    end
+
+    def current_offset
+      # Ensures the offset is at least zero.
+      [ params.fetch(:offset, DEFAULT_OFFSET).to_i, 0 ].max
+    end
+
+    def current_limit
+      # Takes the provided limit or uses a default, capped by a maximum.
+      [ params.fetch(:limit, DEFAULT_LIMIT).to_i, MAX_LIMIT ].min
     end
   end
 end
