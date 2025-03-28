6155d487e6af5ecf7a9a3d2937147a89a5f76b2e
Adrian Bercea <adrian.bercea@autouncle.com>
2025-03-28 23:47:26 +0200

    Add create and delete task buttons

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  include ActionView::RecordIdentifier # For dom_id helper
   before_action :set_task, only: %i[show edit update destroy update_position]
 
   def index
@@ -17,29 +18,49 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  def edit
+  def new
+    @task = Task.new
+    @task.list_id = params[:list_id]
+    @list_id = params[:list_id]
   end
 
   def create
     @task = Task.new(task_params)
-    @task.position = (Task.maximum(:position) || 0).to_i + 1
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to tasks_path, notice: "Task was successfully created." }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
+    @list_id = @task.list_id
+
+    if @task.save
+      # Set position to be at the beginning of the list
+      @task.insert_at(1) # This moves it to the top position in the list
+
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.prepend("list_#{@task.list_id}_tasks", partial: "tasks/task", locals: { task: @task }),
+            turbo_stream.replace("new-task-frame-#{@task.list_id}",
+                                partial: "tasks/new_button",
+                                locals: { list_id: @task.list_id })
+          ]
+        end
+        format.html { redirect_to lists_path }
       end
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def update
     respond_to do |format|
       if @task.update(task_params)
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(dom_id(@task),
+            partial: "tasks/task",
+            locals: { task: @task }
+          )
+        end
         format.html { redirect_to tasks_path, notice: "Task was successfully updated." }
         format.json { render :show, status: :ok, location: @task }
       else
+        format.turbo_stream { render :edit, status: :unprocessable_entity }
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
@@ -47,9 +68,15 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    list_id = @task.list_id # Store the list_id before destroying the task
+
     @task.destroy!
+
     respond_to do |format|
-      format.html { redirect_to tasks_path, status: :see_other, notice: "Task was successfully destroyed." }
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.remove(dom_id(@task))
+      end
+      format.html { redirect_to lists_path, status: :see_other, notice: "Task was successfully destroyed." }
       format.json { head :no_content }
     end
   end
